10475afe5f4d5790af18d02e51c7914588901db9
Joonas Köppä <joonas.koppa@smartly.io>
2023-06-07 22:24:03 +0300

    fix deploy script to handle deletes gracefully

--- aws-deploy.sh
+++ aws-deploy.sh
@@ -1,12 +1,15 @@
 TEMPLATE_FILE="aws/deployment.yaml"
-STACK_NAME="hackathon4"
+STACK_NAME="chainlink-kubernetes-controller"
 
 # Parameters
 IAM_ROLE_NAME="EKSAdmin"
 
-aws cloudformation destroy-stack --stack-name $STACK_NAME
+echo "Deleting stack '$STACK_NAME' if exists..."
+aws cloudformation delete-stack --stack-name "$STACK_NAME"
+aws cloudformation wait stack-delete-complete --stack-name "$STACK_NAME"
+echo "Deploying stack '$STACK_NAME'..."
 aws cloudformation deploy \
 --template-file "$TEMPLATE_FILE" \
 --stack-name "$STACK_NAME" \
---parameter-overrides EKSIAMRoleName="$IAM_ROLE_NAME" \
+--parameter-overrides IAMRoleName="$IAM_ROLE_NAME" \
 --capabilities CAPABILITY_NAMED_IAM
