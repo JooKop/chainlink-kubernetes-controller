7962dba54df84eac9d6b0ea224b2ba7dba2a7ff4
Joonas Köppä <joonas.koppa@smartly.io>
2023-06-07 22:14:49 +0300

    add deploy script

--- aws-deploy.sh
+++ aws-deploy.sh
@@ -1,5 +1,12 @@
+TEMPLATE_FILE="aws/deployment.yaml"
+STACK_NAME="hackathon4"
+
+# Parameters
+IAM_ROLE_NAME="EKSAdmin"
+
+aws cloudformation destroy-stack --stack-name $STACK_NAME
 aws cloudformation deploy \
---template-file aws/deployment.yaml \
---stack-name hackathon4 \
---parameter-overrides EKSIAMRoleName=eksadmin \
+--template-file "$TEMPLATE_FILE" \
+--stack-name "$STACK_NAME" \
+--parameter-overrides EKSIAMRoleName="$IAM_ROLE_NAME" \
 --capabilities CAPABILITY_NAMED_IAM
