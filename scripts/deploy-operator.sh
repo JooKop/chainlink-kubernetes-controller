81dabd2f4a91fa76f42f7f6543bb0c1fc64f3e08
Joonas Köppä <joonas.koppa@smartly.io>
2023-06-09 20:05:31 +0300

    create kubernetes deploy script

--- scripts/deploy-operator.sh
+++ scripts/deploy-operator.sh
@@ -1,1 +1,4 @@
-kubectl apply -f operator/examples/chainlink-kubernetes-operator.yaml
+# This script builds the chainlink-kubernetes-operator project into a Docker image
+# The created image is then pushed into AWS ECR
+export IMAGE_TAG_BASE="310341869582.dkr.ecr.eu-central-1.amazonaws.com/chainlink-kubernetes-operator"
+(cd operator && make deploy)
