2dc18ac9a8c52f583cd3408b4c01cb2dfe0d6a55
Joonas Köppä <joonas.koppa@smartly.io>
2023-06-09 18:55:10 +0300

    initial script for building and pushing image to ECR

---
+++ scripts/aws-build-push.sh
@@ -0,0 +1,6 @@
+# This script builds the chainlink-kubernetes-operator project into a Docker image
+# The created image is then pushed into AWS ECR
+ECR_REGISTRY="310341869582.dkr.ecr.eu-central-1.amazonaws.com/chainlink-kubernetes-operator"
+REGION="eu-central-1"
+
+aws ecr get-login-password --region $REGION | docker login --username AWS --password-stdin $ECR_REGISTRY
