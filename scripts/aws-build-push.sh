a214d36450a77d98ad0c4896906e08e09cf6f299
Joonas Köppä <joonas.koppa@smartly.io>
2023-06-09 19:10:40 +0300

    add push to ECR command

--- scripts/aws-build-push.sh
+++ scripts/aws-build-push.sh
@@ -2,5 +2,8 @@
 # The created image is then pushed into AWS ECR
 ECR_REGISTRY="310341869582.dkr.ecr.eu-central-1.amazonaws.com/chainlink-kubernetes-operator"
 REGION="eu-central-1"
+IMAGE_TAG_BASE="$ECR_REGISTRY/chainlink-kubernetes-controller"
 
 aws ecr get-login-password --region $REGION | docker login --username AWS --password-stdin $ECR_REGISTRY
+
+(cd operator && make docker-build docker-push)
